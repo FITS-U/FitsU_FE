244d1628c3897971118eb0331fe69864abc20cd3
seyeon326 <lseyeon0723@gmail.com>
2024-11-15 10:24:33 +0900

    initial setting

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Fits U",
+  description:
+    "당신의 소비 패턴에 꼭 맞는 최적의 카드를 추천해주는 스마트 카드 매칭 플랫폼",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-300`}
+      >
+        <div className="max-w-[600px] bg-black min-h-screen m-auto h-full overflow-x-hidden scrollbar-hide">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
