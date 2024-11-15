e8bd2bf3995b27c7c173260bd1f2c344efedf98c
seyeon326 <lseyeon0723@gmail.com>
2024-11-15 20:54:47 +0900

    feat: EachCategory component -> props 넘겨받도록 수정 및 구현

--- src/app/my-spend/categories/components/EachCategory.tsx
+++ src/app/my-spend/categories/components/EachCategory.tsx
@@ -1,13 +1,20 @@
 import Image from "next/image";
 
-const EachCategory = () => {
+interface EachCategoryProps {
+  name: string;
+  percentage: string;
+  amount: string;
+  iconSrc: string;
+}
+
+const EachCategory = ({ name, percentage, amount, iconSrc }: EachCategoryProps) => {
   return (
     <div className="mt-7 flex items-center justify-between text-sm">
       <div className="flex items-center justify-between">
-        <Image width={45} height={45} src="/icons/shopping.png" alt="쇼핑 카테고리 로고"/>
+        <Image width={45} height={45} src={iconSrc} alt={`${name} 카테고리 로고`}/>
         <span className="ml-4">
-          <div>쇼핑</div>
-          <div>37.4% | 40,300원</div>
+          <div>{name}</div>
+          <div>{percentage}% | {amount}원</div>
         </span>
       </div>
       <span className="text-lg">&gt;</span>
--- src/app/my-spend/categories/page.tsx
+++ src/app/my-spend/categories/page.tsx
@@ -6,11 +6,11 @@ const Categories = () => {
     <div className="px-8 py-4 text-white">
       <Link href="/my-spend" className="text-3xl">&lt;</Link>
       <div className="my-4 text-lg font-bold">카테고리별 소비</div>
-      <EachCategory />
-      <EachCategory />
-      <EachCategory />
-      <EachCategory />
-      <EachCategory />
+      <EachCategory name="식비" percentage="44.63" amount="127,900" iconSrc="/icons/food.png" />
+      <EachCategory name="교통" percentage="20.94" amount="60,000" iconSrc="/icons/transport.png" />
+      <EachCategory name="쇼핑" percentage="14.06" amount="40,300" iconSrc="/icons/shopping.png" />
+      <EachCategory name="카페" percentage="13.12" amount="37,600" iconSrc="/icons/cafe.png" />
+      <EachCategory name="취미" percentage="7.26" amount="20,800" iconSrc="/icons/hobby.png" />
     </div>
   );
 };
