dc29815117448a9ebc092e5b07646abf89bac2f6
seyeon326 <lseyeon0723@gmail.com>
2024-12-09 13:09:19 +0900

    fix: 카드 혜택 카테고리 선언 오류 해결

--- src/app/recommends/components/ScrollBenefits.tsx
+++ src/app/recommends/components/ScrollBenefits.tsx
@@ -8,47 +8,10 @@ import { useEffect, useRef, useState } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 
 export const ScrollBenefits = () => {
-  // 임시데이터
-  const category = [
-    { id: 1, name: "항공마일리지" },
-    { id: 2, name: "쇼핑" },
-    { id: 3, name: "간편결제" },
-    { id: 4, name: "포인트/캐시백" },
-    { id: 5, name: "편의점" },
-    { id: 6, name: "대형마트" },
-    { id: 7, name: "카페/베이커리" },
-    { id: 8, name: "납부 혜택" },
-    { id: 9, name: "외식" },
-    { id: 10, name: "의료" },
-    { id: 11, name: "반려동물" },
-    { id: 12, name: "뷰티" },
-    { id: 13, name: "대중교통" },
-    { id: 14, name: "주유" },
-    { id: 15, name: "하이패스" },
-    { id: 16, name: "교육" },
-    { id: 17, name: "육아" },
-    { id: 18, name: "문화" },
-    { id: 19, name: "레저" },
-    { id: 20, name: "영화" },
-    { id: 21, name: "통신" },
-    { id: 22, name: "관리비" },
-    { id: 23, name: "Priority Pass" },
-    { id: 24, name: "프리미엄" },
-    { id: 25, name: "오토" },
-    { id: 26, name: "금융" },
-    { id: 27, name: "체크카드겸용" },
-    { id: 28, name: "바우처" },
-    { id: 29, name: "언제나할인" },
-    { id: 30, name: "렌탈" },
-    { id: 31, name: "경차유류환급" },
-    { id: 32, name: "연회비지원" },
-    { id: 33, name: "국민행복카드" },
-    { id: 34, name: "그린카드" },
-  ];
   const [categories, setCategories] = useState<Category[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [isDropDownOpen, setIsDropDownOpen] = useState<boolean>(false);
-  const [selectedBenefit, setSelectedBenefit] = useState(categories[0].categoryId);
+  const [selectedBenefit, setSelectedBenefit] = useState<number | null>(null);
   const scrollRefs = useRef<(HTMLDivElement | null)[]>([]);
   const { user } = useAuthStore();
 
@@ -57,6 +20,11 @@ export const ScrollBenefits = () => {
       try {
         const data = await getCategories();
         setCategories(data);
+
+        // 첫 번째 카테고리를 기본 선택값으로 설정
+        if (data.length > 0) {
+          setSelectedBenefit(data[0].categoryId);
+        }
       } catch (error) {
         console.error("Failed to fetch category list:", error);
       } finally {
@@ -76,7 +44,7 @@ export const ScrollBenefits = () => {
     setIsDropDownOpen(false);
 
     // 해당 카테고리를 가운데로 스크롤
-    const selectedIndex = categories.findIndex((cat) => cat.categoryId === id);
+    const selectedIndex = categories.findIndex((cat) => id === cat.categoryId);
     scrollRefs.current[selectedIndex]?.scrollIntoView({
       behavior: "smooth",
       block: "nearest",
