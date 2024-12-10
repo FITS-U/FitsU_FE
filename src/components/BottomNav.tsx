55356b78cebef4dddbf0ae19118ae45d6ea27b05
seyeon326 <lseyeon0723@gmail.com>
2024-12-10 13:20:08 +0900

    chore

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { FaHouse, FaChartPie, FaBoltLightning, FaCreditCard } from "react-icons/fa6";
-import { IoSettingsSharp } from "react-icons/io5";
 
 const BottomNav = () => {
   const currentPath = usePathname();
